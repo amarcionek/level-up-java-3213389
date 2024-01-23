38636ab8db25856011206b9df9d89ddb4125539d
Adam Marcionek <amarcionek@users.noreply.github.com>
2024-01-23 20:33:58 +0000

    done

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -4,8 +4,35 @@ import java.util.Scanner;
 
 public class App {
 
+    /*
+     * 6 chars, 1 upper, 1 lower, 1 number
+     */
     public static boolean isPasswordComplex(String password) {
-        return false;
+
+        return password.length() >= 6
+                && password.matches(".*\\d.*")
+                && password.matches(".*[a-z].*")
+                && password.matches(".*[A-Z].*");
+
+        // boolean hasNumber = false;
+        // boolean hasUpper = false;
+        // boolean hasLower = false;
+
+        // for (int i = 0; i < password.length(); i++) {
+        // char ch = password.charAt(i);
+        // if (Character.isDigit(ch)) {
+        // hasNumber = true;
+        // } else if (Character.isUpperCase(ch)) {
+        // hasUpper = true;
+        // } else if (Character.isLowerCase(ch)) {
+        // hasLower = true;
+        // }
+
+        // if (hasNumber && hasUpper && hasLower)
+        // return true;
+
+        // }
+        // return false;
     }
 
     public static void main(String[] args) {
