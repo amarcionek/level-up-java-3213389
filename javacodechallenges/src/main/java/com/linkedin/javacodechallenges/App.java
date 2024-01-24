54cba094263296e5f252ababcb3892dff2b1a056
Adam Marcionek <amarcionek@users.noreply.github.com>
2024-01-24 01:29:56 +0000

    done

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -4,9 +4,21 @@ import java.util.Scanner;
 
 public class App {
 
+    // Min charge 18.84 for 1496 gallows (2 CCFs)
+    // One CCF = 748 gallons
+    // > 2 CCFs, + 3.90 per additional CCF
     public static double calculateWaterBill(double gallonsUsage) {
-        // TODO: Implement method
-        return 0;
+
+        double perCCF = 18.84 / 2;
+        double cost = 2 * perCCF;
+
+        double numCCFs = Math.ceil(gallonsUsage / 748);
+
+        if (numCCFs > 2) {
+            cost += (numCCFs - 2) * 3.90;
+        }
+
+        return cost;
     }
 
     public static void main(String[] args) {
