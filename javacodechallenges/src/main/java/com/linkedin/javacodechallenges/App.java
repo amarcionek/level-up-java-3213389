c79d2c6266f968f12c6792435ef221d0ad189b2b
Adam Marcionek <amarcionek@users.noreply.github.com>
2024-01-31 01:55:21 +0000

    done

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,11 +1,33 @@
 package com.linkedin.javacodechallenges;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
+import com.google.gson.Gson;
+
 /**
  * Hello world!
  *
  */
 public class App {
     public static void main(String[] args) {
-        // TODO: Call https://icanhazdadjoke.com/ API and display joke
+        try {
+            HttpRequest req = HttpRequest.newBuilder(new URI("https://icanhazdadjoke.com/"))
+                    .header("Accept", "application/json")
+                    .build();
+
+            HttpClient client = HttpClient.newBuilder().build();
+            HttpResponse<String> response = client.send(req, BodyHandlers.ofString());
+
+            Gson gson = new Gson();
+            Joke joke = gson.fromJson(response.body(), Joke.class);
+
+            System.out.println(joke.getJoke());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
