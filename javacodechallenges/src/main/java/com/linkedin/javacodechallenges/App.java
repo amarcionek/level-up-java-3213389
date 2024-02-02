3bc03c46a040ab4ad1e68da5e3560c8598bbfcd6
Adam Marcionek <amarcionek@users.noreply.github.com>
2024-02-02 14:49:59 +0000

    done

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,12 +1,46 @@
 package com.linkedin.javacodechallenges;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
-public class App 
-{
-    public static void redactTextFile(String fileName,
-                                      String[] redactedWordsArray) {
+import org.apache.commons.lang3.StringUtils;
 
+public class App {
+    public static void redactTextFile(String fileName,
+            String[] redactedWordsArray) {
+
+        File fileRead = new File(fileName);
+        File fileWrite = new File(fileName + "new");
+        Scanner reader = null;
+        FileWriter myWriter = null;
+        try {
+            reader = new Scanner(fileRead);
+            myWriter = new FileWriter(fileWrite);
+
+            while (reader.hasNext()) {
+                String line = reader.nextLine();
+                // Watch for commas in words
+                for (String redacted : redactedWordsArray) {
+                    line = StringUtils.replaceIgnoreCase(line, redacted, "REDACTED");
+                }
+                myWriter.write(line + "\n");
+            }
+            myWriter.flush();
+
+        } catch (Exception e) {
+            if (reader != null)
+                reader.close();
+
+            if (myWriter != null)
+                try {
+                    myWriter.close();
+                } catch (IOException e1) {
+                    // TODO Auto-generated catch block
+                    e1.printStackTrace();
+                }
+        }
     }
 
     public static void main(String[] args) {
